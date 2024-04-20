f1bcf6cdd40bc91aeefea5355ff87595e92efa94
Mohamed Ramadan <mohamed_r_2015@yahoo.com>
2024-04-20 12:07:20 +0200

    Initial Commit

---
+++ PlaySwiftUI/PlaySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaySwiftUIApp.swift
+//  PlaySwiftUI
+//
+//  Created by Mohamed Ramadan on 20/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlaySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
